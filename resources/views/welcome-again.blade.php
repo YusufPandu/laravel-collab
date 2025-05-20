dc9376e224d73b1020d3501eade2c240a5e58883
YusufPandu <yusufpandusatrioaji@gmail.com>
2025-05-20 12:43:05 +0700

    Inisialisasi proyek Laravel oleh admin

---
+++ resources/views/welcome-again.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang=&quot;en&quot;>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+    <h1>Welcome to Laravel!</h1>
+    <p>Welcome again from views</p>
+</body>
+</html>
